058699fb4a16e0442843b107e41eaec363d42180
shinyelee <shinye0213@naver.com>
2020-12-14 13:22:17 +0900

    [Java로 배우는 자료구조] 제1-1장: 변수, 배열, 반복문 (2/7)

---
+++ Lesson01/src/Code04.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Code04 {
+
+	public static void main(String[] args) {
+		
+		String name = null;
+		int age;
+		String gender;
+		
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please type your name and your age and your gender: ");
+		
+		name = kb.next();	// 키보드로부터 문자(단어) 입력받음.
+		age = kb.nextInt();	// 키보드로부터 정수 입력받음.
+		gender = kb.next(); // 키보드로부터 문자(단어) 입력받음.
+		
+		if(gender.equals("male"))		 // if문 중괄호 생략 가능.
+			System.out.println(name + ", you're " + age + "years old man.");
+		else if(gender.equals("female")) // ""로 둘러싼 문자열을 string literal이라고 부른다.
+			System.out.println(name + ", you're " + age + "years old woman.");
+		else							 // if ~ else if ~ else로 연속 비교 가능.
+			System.out.println("Hmm... interesting.");
+		
+		kb.close();
+	}
+
+}
