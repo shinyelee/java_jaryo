3513001664630c902bcae82aab6bdd09ebf42d10
shinyelee <shinye0213@naver.com>
2020-12-14 19:54:57 +0900

    [Java로 배우는 자료구조] 제1-1장: 변수, 배열, 반복문 (3/7)

--- Lesson01/src/Code05.java
+++ Lesson01/src/Code05.java
@@ -3,19 +3,17 @@ public class Code05 {
 	
 	public static void main(String[] args) {
 		
-		// 크기가 5인 정수 배열 만들기.
-		
 		// 크기 5인 정수형 배열.
 		int [] grades;		  // 배열 선언(declare the array)
-		grades = new int [5]; // 배열 생성(allocate memory for 5 indices)
+		grades = new int[5]; // 배열 생성(allocate memory for 5 indices)
 		
 		// 크기 100인 실수형 배열.
 		double [] array;
-		array = new double [100];
+		array = new double[100];
 		
 		// 크기 20인 문자형 배열.
 		char [] word;
-		word = new char [20];
+		word = new char[20];
 		
 		// 아래처럼 표기해도 됨.
 //		int [] grades = new int [5];
@@ -23,7 +21,7 @@ public class Code05 {
 //		char [] word = new char [20];
 		
 		// 배열의 각칸에 데이터 저장(assign some values to the array)
-		grades[0] = 100; // 배열의 인덱스틑 1이 아닌 0부터 시작한다.
+		grades[0] = 100; // 배열의 인덱스는 1이 아닌 0부터 시작한다.
 		grades[1] = 76;
 		grades[2] = 92;
 		grades[3] = 95;
