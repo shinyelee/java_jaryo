495e781da8a94ef77b79a4de8053bb5934369236
shinyelee <shinye0213@naver.com>
2020-12-16 02:39:45 +0900

    [Java로 배우는 자료구조] 제1-1장: 변수, 배열, 반복문 (3/7) Code07 설명 추가

--- Lesson01/src/Code07.java
+++ Lesson01/src/Code07.java
@@ -12,12 +12,15 @@ public class Code07 {
 		grades[4] = 14;
 		
 		int i = 0;
-		// 이번엔 while문을 썼다.
+		
+		// while은 for과 함께 대표적인 반복문의 하나다.
 		while(i<grades.length) { // 여기 선언한 변수 i는 for문 안에서만 적용.
 			System.out.println("Grade " + (i+1) + ": " + grades[i]);
 			i++;
 		}
 
+		// 모든 for문은 while문으로 바꿀 수 있고, 그 역도 마찬가지다.
+		// 일반적을오 반복의 횟수가 정해져 있을 경우 for문을, 그렇지 않을 경우 while문을 선호한다.
 	}
 
 }
