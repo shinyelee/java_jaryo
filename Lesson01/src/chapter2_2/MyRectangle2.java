89dd3df14f8242c281790d854ee3aa970ec96d58
shinyelee <shinye0213@naver.com>
2020-12-29 15:37:57 +0900

    [Java로 배우는 자료구조] 제2-2장: 메서드와 생성자 (2/3) 설명 추가

--- Lesson01/src/chapter2_2/MyRectangle2.java
+++ Lesson01/src/chapter2_2/MyRectangle2.java
@@ -6,6 +6,11 @@ public class MyRectangle2 {
 	public int width;
 	public int height;
 	
+	// 하나의 클래스가 2개 이상의 생성자를 가질 수 있다.
+	// 단, 생성자들은 매개변수의 개수나 타입에 있어서 차이가 있어야 한다.
+	// 객체를 생성할 때 어떤 매개변수를 제공하느냐에 따라서
+	// 해당되는 생성자가 선택되어 실행한다.
+	
 	public MyRectangle2(int x, int y, int w, int h) {
 		lu = new MyPoint2(x, y);
 		width = w;
