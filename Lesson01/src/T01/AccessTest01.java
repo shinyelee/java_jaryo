d75363fd41870ed4f009c2c4ca33ad9b70c6afae
shinyelee <shinye0213@naver.com>
2020-12-31 17:44:18 +0900

    [접근 제어자] (default)

--- Lesson01/src/T01/AccessTest01.java
+++ Lesson01/src/T01/AccessTest01.java
@@ -7,7 +7,9 @@ public class AccessTest01 {
 		Access01 ac = new Access01();
 		System.out.println(ac.a);
 		System.out.println(ac.b);
-
+		
+		AccessTest02 act02 = new AccessTest02();
+		System.out.println(act02.c);
 	}
 
 }
@@ -20,5 +22,9 @@ class Access01 {
 //	private int b = 100;
 	// private은 같은 클래스(Access01) 내에서만 사용 가능.
 	// -> Access01 외 클래스에서 사용하면 에러 발생.
+	
+	// 변수 a, b에 따로 접근 제어자를 지정하지 않음
+	// -> 자동으로 (default) 적용
+	// -> 같은 패키지 내에서만 접근 가능.
 	
 }
---
+++ Lesson01/src/T01/AccessTest02.java
@@ -0,0 +1,15 @@
+package T01;
+
+public class AccessTest02 {
+
+	int c = 300;
+	int d = 400;
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
+
+// AccessTest01, AccessTest02는 T01이라는 패키지에 들어있음
+// -> 둘 다 같은 패키지라 변수 c, d에 모두 접근 가능.
