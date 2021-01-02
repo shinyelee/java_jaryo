ec86e9fe70b04e8a2bc0ecd66210a9572c2431f1
shinyelee <shinye0213@naver.com>
2021-01-02 23:18:06 +0900

    [Java로 배우는 자료구조] 제2-3장: static 그리고 public (2/2) expo 캡슐화

--- Lesson01/src/chapter2_3/Polynomial3.java
+++ Lesson01/src/chapter2_3/Polynomial3.java
@@ -2,20 +2,11 @@ package chapter2_3;
 
 public class Polynomial3 {
 
-	// 객체지향 프로그래밍에서 객체란 "데이터"+"메서드"다.
-	// 데이터는 객체의 정적 속성을 표현하며, 메서드는 객체의 동적 속성을 표현한다.
-	
-	// 자전거는 "모양, 무게, 크기, 브랜드" 등의 정적 속성(데이터)과
-	// "달린다, 정지한다, 뒤로 간다" 등의 동적 속성(기능)을 가진다.
-	// -> 하나의 항(term)은 "계수"와 "차수"라는 정적 속성과
-	// "x의 값을 주면 자신의 값을 계산해준다", "화면에 출력해준다" 등의 기능을 가진다.
-	
 	public char name;
 	public Term3 [] terms;
-	public int nTerms; // 객체의 정적 속성(데이터)
+	public int nTerms;
 	
-	public Polynomial3() { // 생성자가 반드시 매개변수를 받아야 하는 건 아니다.
-		// 생성자는 객체에게 필요한 초기화 작업을 하기에 적절한 장소다.
+	public Polynomial3() { 
 		nTerms = 0;
 		terms = new Term3[100];
 	}
@@ -26,14 +17,14 @@ public class Polynomial3 {
 		terms = new Term3[100];
 	}
 	
-	public void addTerm(int c, int e) { // 객체의 동적 속성(기능)
+	public void addTerm(int c, int e) {
 		int index = findTerm(e);
 		if(index != -1) {
 			terms[index].coef += c;
 		}
 		else {
 			int i = nTerms-1;
-			while(i>=0 && terms[i].expo<e) {
+			while(i>=0 && terms[i].getExpo()<e) {
 				terms[i+1] = terms[i];
 				i--;
 			}
@@ -43,8 +34,8 @@ public class Polynomial3 {
 	}
 	
 	public int findTerm(int e) {
-		for(int i=0; i<nTerms && terms[i].expo>=e; i++)
-			if(terms[i].expo == e)
+		for(int i=0; i<nTerms && terms[i].getExpo()>=e; i++)
+			if(terms[i].getExpo() == e)
 				return i;
 		return -1;
 	}
--- Lesson01/src/chapter2_3/Term3.java
+++ Lesson01/src/chapter2_3/Term3.java
@@ -3,23 +3,22 @@ package chapter2_3;
 public class Term3 {
 	
 	public int coef;
-	public int expo;
+	private int expo; // 캡슐화
 	
-	// 클래스 안에 클래스와 동일한 이름을 가지며 return 타입이 없는 메서드를
-	// 생성자(constructor)라고 한다.
-	// 생성자는 new 명령으로 객체가 생성될 때 자동으로 실행된다.
-	// 주 목적은 객체의 데이터 필드 값을 초기화하는 것이다.
-
-	public Term3(int c, int e) { // 생성자(리턴 타입 없음)
+	public Term3(int c, int e) {
 		coef = c;
 		expo = e;
 	}
 	
-	public int calcTerm(int x) { // 리턴 타입 : int
+	public int getExpo() { // getter method
+		return expo;	   // 읽기(O) 수정(x)
+	}					   // 의도치 않은 오류 방지
+	
+	public int calcTerm(int x) {
 		return (int) (coef * Math.pow(x, expo));
 	}
 
-	public void printTerm() { // 리턴 타입 : void
+	public void printTerm() {
 		System.out.print(coef + "x^" + expo);
 	}
 
