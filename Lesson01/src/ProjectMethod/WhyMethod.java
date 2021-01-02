351ffc24b3ef8d5ec1743bd6d4acbea31052444a
shinyelee <shinye0213@naver.com>
2021-01-03 01:04:51 +0900

    [생활코딩 JAVA method] 4. 메소드의 입력 2

--- Lesson01/src/ProjectMethod/WhyMethod.java
+++ Lesson01/src/ProjectMethod/WhyMethod.java
@@ -2,18 +2,19 @@ package ProjectMethod;
 
 public class WhyMethod {
 
-	public static void printTwoTimes(String text) {
-		System.out.println("-");
-		System.out.println(text); // 출력하는 문자를 자유롭게 변경하려면
+	public static void printTwoTimes(String text, String delimiter) {
+		System.out.println(delimiter); // 특수문자도
 		System.out.println(text);
+		System.out.println(text);
+		// delimiter, text -> 매개변수, parameter
 	}
 	
 	public static void main(String[] args) {
-		printTwoTimes("a"); // ""에 원하는 문자를 입력하면 된다.
-		printTwoTimes("a");
-		printTwoTimes("a");
-		printTwoTimes("b");
-
+		printTwoTimes("a", "-"); // 자유롭게 변경 가능.
+		printTwoTimes("a", "*");
+		printTwoTimes("a", "&");
+		printTwoTimes("b", "!");
+		// "a", "-" -> 인자, argument
 	}
 
 }
