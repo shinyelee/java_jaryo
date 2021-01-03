ed2c21e83e3497cee56d4f771938d0942a28e15e
shinyelee <shinye0213@naver.com>
2021-01-03 23:26:03 +0900

    [생활코딩 JAVA method] 부록 1 - access level modifier

---
+++ Lesson01/src/ProjectMethod/AccessLevelModifiersMethod.java
@@ -0,0 +1,15 @@
+package ProjectMethod;
+
+class Greeting{
+    //public, protected, default, private
+    public static void hi() {
+        System.out.println("Hi");
+    }
+}
+
+public class AccessLevelModifiersMethod {
+	public static void main(String[] args) {
+        Greeting.hi();
+    }
+
+}
