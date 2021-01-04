78f66dec130fae675ee0b1e79cc99fa9566a6c55
shinyelee <shinye0213@naver.com>
2021-01-04 23:37:26 +0900

    [생활코딩 JAVA 객체 지향 프로그래밍] 8.1. 활용 - 클래스화

--- Lesson01/src/projectMethod/AccountingApp.java
+++ Lesson01/src/projectMethod/AccountingApp.java
@@ -1,26 +1,27 @@
 package projectMethod;
 
-public class AccountingApp {
+class Accounting { // 클래스로 연관된 메소드끼리 정리정돈.
 	
-	// 공급가액
     public static double valueOfSupply = 10000.0;
-    // 부가가치세율
     public static double vatRate = 0.1;
  
     public static double getVAT() {
         return valueOfSupply * vatRate;
     }
-     
+    
     public static double getTotal() {
         return valueOfSupply + getVAT();
     }
- 
+    
+}
+
+public class AccountingApp {
+
     public static void main(String[] args) {
  
-        System.out.println("Value of supply : " + valueOfSupply);
-        System.out.println("VAT : " + getVAT());
-        System.out.println("Total : " + getTotal());
- 
+        System.out.println("Value of supply : " + Accounting.valueOfSupply);
+        System.out.println("VAT : " + Accounting.getVAT());
+        System.out.println("Total : " + Accounting.getTotal());
     }
 
 }
