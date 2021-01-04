b82052fae8d92907f76ae60d883d6357e4ebac0d
shinyelee <shinye0213@naver.com>
2021-01-04 23:30:14 +0900

    [생활코딩 JAVA 객체 지향 프로그래밍] 7. 생성자와 this

--- Lesson01/src/projectOOP/MyOOP.java
+++ Lesson01/src/projectOOP/MyOOP.java
@@ -4,15 +4,13 @@ public class MyOOP {
 	
 	public static void main(String[] args) {
 		
-		Print p1 = new Print();
-		p1.delimiter = "---";
-		p1.A();
-		p1.B();
-		
-		Print p2 = new Print();
-		p2.delimiter = "***";
-		p2.A();
-		p2.B();		
+		Print p1 = new Print("---");
+//		p1.A();
+//		p1.B();
+//		
+//		Print p2 = new Print("***");
+//		p2.A();
+//		p2.B();
 	}
 
 }
--- Lesson01/src/projectOOP/Print.java
+++ Lesson01/src/projectOOP/Print.java
@@ -2,16 +2,20 @@ package projectOOP;
 
 class Print { // Print 클래스 파일로 따로 분리함.
 
-public static String delimiter = "";
+	public static String delimiter = "";
+	
+	public Print(String delimiter) {
+		this.delimiter = delimiter;
+	}
 	
 	public void A() {
-		System.out.print(delimiter);
+		System.out.print(this.delimiter);
 		System.out.print("A");
 		System.out.println("A");
 	}
 	
 	public void B() {
-		System.out.print(delimiter);
+		System.out.print(this.delimiter);
 		System.out.print("B");
 		System.out.println("B");
 	}
