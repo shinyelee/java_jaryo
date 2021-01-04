c15f79d87f6be8f710aeccc5a86d7437b6926d84
shinyelee <shinye0213@naver.com>
2021-01-04 22:42:02 +0900

    [생활코딩 JAVA 객체 지향 프로그래밍] 4.1. 클래스 - 존재 이유와 기본형식

--- Lesson01/src/projectOOP/MyOOP.java
+++ Lesson01/src/projectOOP/MyOOP.java
@@ -1,29 +1,32 @@
 package projectOOP;
 
-public class MyOOP {
+class Print {
 	
-	public static void main(String[] args) {
-		delimiter = "---";
-		printA();
-		printB();
-		
-		delimiter = "***";
-		printA();
-		printB();
-	}
-
 	public static String delimiter = "";
 	
-	public static void printA() {
+	public static void A() {
 		System.out.print(delimiter);
 		System.out.print("A");
 		System.out.println("A");
 	}
 	
-	public static void printB() {
+	public static void B() {
 		System.out.print(delimiter);
 		System.out.print("B");
 		System.out.println("B");
+	}
+}
+
+public class MyOOP {
+	
+	public static void main(String[] args) {
+		Print.delimiter = "---";
+		Print.A();
+		Print.B();
+		
+		Print.delimiter = "***";
+		Print.A();
+		Print.B();
 	}
 
 }
