f895bf399394d67bbe11e57dc571f7608edec1d5
shinyelee <shinye0213@naver.com>
2021-01-05 21:01:13 +0900

    [생활코딩 JAVA 상속] 3. Overriding vs Overloading

--- Lesson01/src/projectInheritance/InheritanceApp.java
+++ Lesson01/src/projectInheritance/InheritanceApp.java
@@ -1,36 +1,42 @@
 package projectInheritance;
 
 class Cal{
-	
-    int v1,v2;
-    Cal(int v1, int v2) {
-        System.out.println("Cal init!!");
-        this.v1 = v1; this.v2 = v2;
+    public int sum(int v1, int v2){
+        return v1+v2;
     }
-    public int sum() {
-    	return this.v1+v2;
+    // Overloading
+    public int sum(int v1, int v2, int v3){
+        return v1+v2+v3;
     }
 }
-
-// 부모가 가지고 있는 기능을 재정의하는 것을
-// -> 오버라이딩(Overriding)이라고 함.
-class Cal3 extends Cal {
-	
-    Cal3(int v1, int v2) {
-        super(v1, v2);
-        System.out.println("Cal3 init!!");
+class Cal3 extends Cal{
+    public int minus(int v1, int v2){
+        return v1-v2;
     }
-    public int minus() {
-    	return this.v1-v2;
+    // Overriding
+    public int sum(int v1, int v2){
+        System.out.println("Cal3!!");
+        return v1+v2;
     }
+ 
 }
-
 public class InheritanceApp {
-	
-    public static void main(String[] args) {  	
-        Cal c = new Cal(2,1);
-        Cal3 c3 = new Cal3(2, 1);
-        System.out.println(c3.sum()); // 3
-        System.out.println(c3.minus()); // 1
+    public static void main(String[] args) {
+        Cal c = new Cal();
+        System.out.println(c.sum(2,1));
+        System.out.println(c.sum(2,1,1));
+ 
+        Cal3 c3 = new Cal3();
+        System.out.println(c3.sum(2,1));
+        System.out.println(c3.minus(2,1));
+        System.out.println(c3.sum(2,1));
+    }
+}
+class Cal2{
+    public int sum(int v1, int v2){
+        return v1+v2;
+    }
+    public int minus(int v1, int v2){
+        return v1-v2;
     }
 }
