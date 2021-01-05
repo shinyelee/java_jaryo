3ad91fb4fa82b6888641e8c94102db4c7f5b74ac
shinyelee <shinye0213@naver.com>
2021-01-05 20:44:46 +0900

    [생활코딩 JAVA 상속] 2. 기능의 개선과 발전

--- Lesson01/src/projectInheritance/InheritanceApp.java
+++ Lesson01/src/projectInheritance/InheritanceApp.java
@@ -1,34 +1,36 @@
 package projectInheritance;
 
-class Cal {
-	public int sum(int v1, int v2) {
-		return v1 + v2;
-	}
+class Cal{
+	
+    int v1,v2;
+    Cal(int v1, int v2) {
+        System.out.println("Cal init!!");
+        this.v1 = v1; this.v2 = v2;
+    }
+    public int sum() {
+    	return this.v1+v2;
+    }
 }
 
-class Cal3 extends Cal { // Cal 클래스를 확장한다는 의미.
-	// 변수와 메소드를 상속해 다른 클래스가 갖게 함.
-	// -> 재사용성↑ 유지보수↑ 가독성↑ 코드양↓
+// 부모가 가지고 있는 기능을 재정의하는 것을
+// -> 오버라이딩(Overriding)이라고 함.
+class Cal3 extends Cal {
+	
+    Cal3(int v1, int v2) {
+        super(v1, v2);
+        System.out.println("Cal3 init!!");
+    }
+    public int minus() {
+    	return this.v1-v2;
+    }
 }
 
 public class InheritanceApp {
-
-	public static void main(String[] args) {
-		Cal c = new Cal();
-		System.out.println("c : " + c.sum(2, 1));
-		
-		Cal c3 = new Cal3(); // Cal 클래스 상속됨.
-		System.out.println("c3 : " + c3.sum(2, 1));
-	}
-
-}
-
-class Cal2 {
-	public int sum(int v1, int v2) { // Cal 클래스와 중복인 코드.
-		return v1 + v2;
-	}
 	
-	public int minus(int v1, int v2) {
-		return v1 - v2;
-	}
+    public static void main(String[] args) {  	
+        Cal c = new Cal(2,1);
+        Cal3 c3 = new Cal3(2, 1);
+        System.out.println(c3.sum()); // 3
+        System.out.println(c3.minus()); // 1
+    }
 }
