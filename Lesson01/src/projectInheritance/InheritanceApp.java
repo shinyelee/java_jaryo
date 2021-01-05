f331edcc78f8012c26239a9799405598191a5b6b
shinyelee <shinye0213@naver.com>
2021-01-05 22:30:16 +0900

    [생활코딩 JAVA 상속] 5. 상속과 생성자

--- Lesson01/src/projectInheritance/InheritanceApp.java
+++ Lesson01/src/projectInheritance/InheritanceApp.java
@@ -1,42 +1,33 @@
 package projectInheritance;
 
 class Cal{
-    public int sum(int v1, int v2){
-        return v1+v2;
-    }
-    // Overloading
-    public int sum(int v1, int v2, int v3){
-        return this.sum(v1, v2)+v3;
-    } // this는 자기 자신 내 메소드/필드 지칭(가독성↑).
+	int v1, v2;
+	Cal(int v1, int v2) {
+		System.out.println("Cal init!!");
+		this.v1 = v1; this.v2 = v2;
+	}
+	public int sum() { // 생성자
+		return this.v1+v2;
+	}
 }
+
 class Cal3 extends Cal{
-    public int minus(int v1, int v2){
-        return v1-v2;
-    }
-    // Overriding
-    public int sum(int v1, int v2){
-        System.out.println("Cal3!!");
-        return super.sum(v1, v2);
-    } // super는 부모 클래스 내 메소드/필드 지칭(가독성↑).
- 
+	Cal3(int v1, int v2) {
+		super(v1, v2);
+		// 생성자가 있는 부모 클래스를 상속받으려면
+		// -> 생성자도 함께 상속 받아야 한다.
+		System.out.println("Cal3 init!!");
+	}
+	public int minus() { // 생성자
+		return this.v1-v2;
+	}
 }
+
 public class InheritanceApp {
-    public static void main(String[] args) {
-        Cal c = new Cal();
-        System.out.println(c.sum(2,1));
-        System.out.println(c.sum(2,1,1));
- 
-        Cal3 c3 = new Cal3();
-        System.out.println(c3.sum(2,1));
-        System.out.println(c3.minus(2,1));
-        System.out.println(c3.sum(2,1));
-    }
-}
-class Cal2{
-    public int sum(int v1, int v2){
-        return v1+v2;
-    }
-    public int minus(int v1, int v2){
-        return v1-v2;
-    }
+	public static void main(String[] args) {
+    	Cal c = new Cal(2, 1);
+    	Cal3 c3 = new Cal3(2, 1);
+    	System.out.println(c3.sum()); // 3
+    		System.out.println(c3.minus()); // 1
+	}
 }
