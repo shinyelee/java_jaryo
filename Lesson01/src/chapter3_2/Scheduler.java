da636ddb1cd3ff28657e1e875025d422b69e2e0d
shinyelee <shinye0213@naver.com>
2021-01-08 23:37:29 +0900

    [Java로 배우는 자료구조] 제3-2장: 예제 (Scheduler Program) (2/2)

--- Lesson01/src/chapter3_2/Scheduler.java
+++ Lesson01/src/chapter3_2/Scheduler.java
@@ -3,19 +3,27 @@ package chapter3_2;
 import java.util.Scanner;
 
 public class Scheduler {
-	public Event [] events = new Event [100];
+	private int capacity = 10;
+	public Event [] events = new Event [capacity];
 	public int n = 0;
+	private Scanner kb;
 	
 	public void processCommand() {
-		Scanner kb = new Scanner(System.in);
+		kb = new Scanner(System.in);
 		while(true) {
 			System.out.print("$ ");
 			String command = kb.next();
 			if(command.equals("addEvent")) {
-				
+				String type = kb.next();
+				if(type.equalsIgnoreCase("oneday"))
+					handleAddOneDayEvent();
+				else if(type.equalsIgnoreCase("duration"))
+					handleAddDurationEvent();
+				else if(type.equalsIgnoreCase("deadline"))
+					handleAddDeadlinedEvent();
 			}
 			else if(command.equals("list")) {
-				
+				handleList();
 			}
 			else if(command.equals("show")) {
 				
@@ -26,6 +34,60 @@ public class Scheduler {
 		kb.close();
 	}
 	
+	private void handleList() {
+		for(int i=0; i<n; i++)
+			System.out.println("  " + events[i].toString()); // dynamic binding	
+	}
+
+	private void handleAddDeadlinedEvent() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void handleAddDurationEvent() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void handleAddOneDayEvent() {
+		System.out.print(" when: ");
+		String dateString = kb.next(); // dateString = "2020/01/20"
+		System.out.print(" title: ");
+		String title = kb.next();
+		
+		MyDate date = parseDateString(dateString);
+		OneDayEvent ev = new OneDayEvent(title, date);
+//		System.out.println(ev.toString());
+		addEvent(ev);
+	}
+
+	private void addEvent(OneDayEvent ev) {
+		if(n>= capacity)
+			reallocate();
+		
+		events[n++] = ev;
+	}
+
+	private void reallocate() {
+		Event [] tmp = new Event [capacity*2];
+		for(int i=0; i<n; i++)
+			tmp[i] = events[i];
+		events = tmp;
+		capacity *= 2;
+	}
+
+	private MyDate parseDateString(String dateString) { // dateString = "2020/01/20"
+		String [] tokens = dateString.split("/");
+		// tokens[0] = "2020";
+		
+		int year = Integer.parseInt(tokens[0]); // 문자 <-> 정수 변환.
+		int month = Integer.parseInt(tokens[1]);
+		int day = Integer.parseInt(tokens[2]);
+		
+		MyDate d = new MyDate(year, month, day);
+		return d;
+	}
+
 	public static void main(String[] args) {
 		Scheduler app = new Scheduler();
 		app.processCommand();
