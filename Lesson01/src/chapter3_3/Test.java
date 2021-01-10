ccb1f6e002418329181f639b9e990f73b4526bd8
shinyelee <shinye0213@naver.com>
2021-01-10 11:02:20 +0900

    [Java로 배우는 자료구조] 제3-3장: 클래스 Object와 Wrapper 클래스

--- Lesson01/src/chapter3_3/Test.java
+++ Lesson01/src/chapter3_3/Test.java
@@ -8,21 +8,35 @@ public class Test { // subclass of Object
 //		return a + " " + x;
 //	}
 
-	public boolean equals(Test other) {
-		return a==other.a && x==other.x;
+	public boolean equals(Object other) { // overriding
+		Test other2 = (Test)other; // type casting
+		return a==other2.a && x==other2.x;
 	}
 	
 	public static void main(String[] args) {
-		Test test1 = new Test();
-		Test test2 = new Test();
+//		Test test1 = new Test();
+//		Test test2 = new Test();
 //		test2.a = 5;
 //		test2.x = 9.8765;
 		
-		System.out.println(test1.toString());
+//		System.out.println(test1.toString());
+//		
+//		if(test2.equals(test1))
+//			System.out.println("yes");
+//		else
+//			System.out.println("no");
 		
-		if(test2.equals(test1))
-			System.out.println("yes");
-		else
-			System.out.println("no");
+	Object [] array = new Object[100];
+	int a = 20;
+//	Integer age = new Integer(a); // wrapping
+	array[0] = a; // autoboxing
+	
+	int b = (Integer)array[0]; // auto unboxing, unwrapping
+	
+	System.out.println(b);
+	
+	String str = "1234";
+	int c = Integer.parseInt(str);
+	System.out.println(c);
 	}
 }
