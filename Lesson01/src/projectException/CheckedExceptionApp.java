07c2d287d379f11ca2cac70fee815732e302a3e6
shinyelee <shinye0213@naver.com>
2021-01-10 17:51:20 +0900

    [생활코딩 JAVA 예외] 7. finally와 resource 다루기

--- Lesson01/src/projectException/CheckedExceptionApp.java
+++ Lesson01/src/projectException/CheckedExceptionApp.java
@@ -1,17 +1,29 @@
 package projectException;
 
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class CheckedExceptionApp {
 
 	public static void main(String[] args) {
-		
+		FileWriter f = null;
 		try {
-			FileWriter f = new FileWriter("data.txt");
+			f = new FileWriter("data.txt");
 			f.write("Hello");
-			f.close();
-		} catch (Exception e) {
+			// close를 하기 전 예외가 발생할 수 있기 때문에
+			// -> finally로 처리해야 한다.
+		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			// 만약에 f가 null이 아니라면
+			if(f!=null) {
+				try {
+					f.close();
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 		
 	}
