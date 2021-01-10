1e6aa5c4c7c34a8f24ae51ce32d83d602392a4f9
shinyelee <shinye0213@naver.com>
2021-01-10 23:47:20 +0900

    [생활코딩 JAVA 예외] 9. 수업을 마치며

---
+++ Lesson01/src/projectException/MyException.java
@@ -0,0 +1,9 @@
+package projectException;
+
+public class MyException {
+
+	public static void main(String[] args) {
+		throw new RuntimeException("문제가 발생했습니다.");
+	}
+
+}
---
+++ Lesson01/src/projectException/ThrowException.java
@@ -0,0 +1,11 @@
+package projectException;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ThrowException {
+
+	public static void main(String[] args) throws IOException {
+		FileWriter f = new FileWriter("data.txt");
+	}	// try~catch·Î °¨½Îµµ µÊ.
+}
