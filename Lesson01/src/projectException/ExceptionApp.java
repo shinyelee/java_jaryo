9049555fdc12455c3b4d3d85a05cf751f2e963fb
shinyelee <shinye0213@naver.com>
2021-01-10 11:36:33 +0900

    [생활코딩 JAVA 예외] 3. 예외의 처리

--- Lesson01/src/projectException/ExceptionApp.java
+++ Lesson01/src/projectException/ExceptionApp.java
@@ -4,8 +4,20 @@ public class ExceptionApp {
 
 	public static void main(String[] args) {
 		System.out.println(1);
-//		System.out.println(2/0); // ArithmeticException by zero
-		System.out.println(3);
+		int [] scores = {10, 20, 30};
+		
+		try {
+			System.out.println(2);
+			System.out.println(scores[3]); // 여기부터
+			System.out.println(3);
+			System.out.println(2/0);
+			System.out.println(4); // 여기까지 출력 X.
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("존재하지 않는 값입니다.");
+		} catch (ArithmeticException e) {
+			System.out.println("잘못된 계산입니다.");
+		}
+		System.out.println(5);
 	}
 
 }
