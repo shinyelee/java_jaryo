b3bddd5cf2f222ff990b086599bbbe8e1af9499b
shinyelee <shinye0213@naver.com>
2021-01-10 11:23:50 +0900

    [생활코딩 JAVA 예외] 2. 예외의 발생

---
+++ Lesson01/src/projectException/ExceptionApp.java
@@ -0,0 +1,11 @@
+package projectException;
+
+public class ExceptionApp {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+//		System.out.println(2/0); // ArithmeticException by zero
+		System.out.println(3);
+	}
+
+}
